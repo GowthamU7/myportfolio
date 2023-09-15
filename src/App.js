6b1e64ab22a9e940fa4c5f036473e855a17656a7
Gowtham Ullangula <gowthammdb7@gmail.com>
2023-09-15 16:30:10 +0530

    Updated layout

--- src/App.js
+++ src/App.js
@@ -97,7 +97,7 @@ function App() {
         <div className="container-full h-full bg-pink-100 p-4" id="projects">
             
             <div className="flex flex-wrap">
-                <div className="rounded-lg border-pink-300 border-2 shadow-sm max-md:w-full max-lg:w-full max-sm:w-full max-2xl:w-1/3 max-2xl:h-96  w-1/3">
+                <div className="lg:w-full p-4 rounded-lg border-pink-300 border-2 shadow-sm max-md:w-full max-lg:w-full max-sm:w-full max-2xl:w-1/3 max-2xl:h-96  w-1/3">
                     <img alt="x" src={feed} className="shadow-lg w-20 h-20 float-right m-3 rounded-lg"/>
                     <p className="clear-left p-3 m-3 font-mono text-sm">
                         This is a personal project that showcases my skill in MongodbCRUD,aggregations and NodeJS express framework for
@@ -110,7 +110,7 @@ function App() {
                         ● <a href="https://main.dfwasqu2oc736.amplifyapp.com/" target="_blank" className="text-sky-600">Porject Live here"</a>
                     </p>
                 </div>
-                <div className="rounded-lg border-pink-300 border-2 shadow-sm max-md:w-full max-lg:w-full max-sm:w-full max-2xl:h-96 max-2xl:w-1/3 w-1/3">
+                <div className="lg:w-full p-4 rounded-lg border-pink-300 border-2 shadow-sm max-md:w-full max-lg:w-full max-sm:w-full max-2xl:h-96 max-2xl:w-1/3 w-1/3">
                     <img alt="x" src={note} className="rounded-lg w-20 h-20 float-right m-3 shadow-lg"/>
                     <p className="clear-left p-3 m-3 font-mono text-sm">
                         This is a personal project that showcases my skill in ReactJs Library and its hooks like useState and useEffect 
@@ -123,7 +123,7 @@ function App() {
                         ● <a target="_blank" className="text-sky-600" href="https://gowthamu7.github.io/notekeeper/">Check my work "</a>
                     </p>
                 </div>
-                <div className="rounded-lg border-pink-300 border-2 shadow-sm max-md:w-full max-lg:w-full max-sm:w-full max-2xl:h-96 max-2xl:w-1/3 w-1/3">
+                <div className="lg:w-full p-4 rounded-lg border-pink-300 border-2 shadow-sm max-md:w-full max-lg:w-full max-sm:w-full max-2xl:h-96 max-2xl:w-1/3 w-1/3">
                     <img alt="x" src={timer} className="shadow-lg rounded-full w-20 h-20 float-right m-3"/>
                     <p className="clear-left p-3 m-3 font-mono text-sm">
                     This is a personal project that showcases my skill in ReactJs Library and its hooks like useState and useEffect 
@@ -161,7 +161,7 @@ function App() {
                         </div>
                     </div>
                     <div className="m-3 p-1">
-                        <p className="font-mono">MySql</p>
+                        <p className="font-mono">Sql</p>
                         <div className="rounded-xl hover:bg-slate-200 bg-slate-300 h-2 w-full block">
                             <div className="rounded-xl hover:bg-green-500 bg-green-300 h-full w-3/4 block"></div>
                         </div>
@@ -201,6 +201,12 @@ function App() {
                     </div>
                     <div className="m-3 p-1">
                         <p className="font-mono">Tailwind css</p>
+                        <div className="rounded-xl hover:bg-slate-200 bg-slate-300 h-2 w-full block">
+                            <div className="rounded-xl hover:bg-green-500 bg-green-300 h-full w-3/4 block"></div>
+                        </div>
+                    </div>
+                    <div className="m-3 p-1">
+                        <p className="font-mono">Mysql</p>
                         <div className="rounded-xl hover:bg-slate-200 bg-slate-300 h-2 w-full block">
                             <div className="rounded-xl hover:bg-green-500 bg-green-300 h-full w-3/4 block"></div>
                         </div>
